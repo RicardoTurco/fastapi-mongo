487f3bee2a46137962506ab571da1be63984c34c
RicardoTurco <ricturco@gmail.com>
2025-04-01 10:56:33 -0300

    change port to run the application

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app="main:get_app",
         host="0.0.0.0",
-        port=8081,
+        port=8085,
         log_config="log_conf.yaml",
         factory=True,
     )
