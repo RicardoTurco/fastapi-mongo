7317ddd76233b0fb291a99703ed2af0c85b24dfe
RicardoTurco <ricturco@gmail.com>
2025-04-01 11:45:18 -0300

    implementing loading environment variables

---
+++ config.py
@@ -0,0 +1,30 @@
+import logging
+
+from pydantic_settings import BaseSettings
+
+logger = logging.getLogger(__name__)
+
+
+class Settings(BaseSettings):
+    """
+    Application general settings class, which inherits from 'BaseSettings' from pydantic.
+    """
+
+    APP_NAME: str = "fastapi-mongo"
+
+    HOST: str = "localhost"
+    PORT: str = "27017"
+    USERNAME: str = "admin"
+    PASSWORD: str = "pass"
+    DB_NAME: str = "fastapi-mongo"
+
+    class Config:
+        """
+        Configurations of pydantic ...
+        """
+
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
